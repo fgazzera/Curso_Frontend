d8c82ec27ed04a072e67f27755239723bd17bc29
fgazzera <2113461@ucc.edu.ar>
2025-10-14 15:13:35 -0300

    Función: Inicialización de la aplicación de lista de tareas de React con Vite y TypeScript
    - Se añadió el archivo package.json con dependencias y scripts para el desarrollo y la compilación.
    - Se incluyó el SVG del logotipo de Vite en el directorio público.
    - Se creó el archivo App.css para diseñar el diseño de la aplicación.
    - Se implementó el componente principal de la aplicación para renderizar NoteList.
    - Se desarrolló el componente ListItem para la visualización de tareas individuales.
    - Se creó el componente NoteList para gestionar y mostrar tareas con la entrada de nuevas tareas.
    - Se añadieron módulos CSS para diseñar los componentes NoteList y ListItem.
    - Se incluyó el SVG del logotipo de React en el directorio de recursos.
    - Se configuró el punto de entrada principal en main.tsx para renderizar la aplicación.
    - Se configuró TypeScript con archivos tsconfig para entornos de aplicaciones y nodos.
    - Se estableció la configuración de Vite para el proyecto.

--- AngularCV/src/app/app.config.ts
+++ AngularCV/src/app/app.config.ts
@@ -5,11 +5,22 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { httpInterceptor } from './http-interceptor';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withInterceptors([httpInterceptor])),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
---
+++ React/lista-tareas/src/App.tsx
@@ -0,0 +1,11 @@
+import NoteList from "./NoteList/NoteList"
+
+function App() {
+  return (
+    <div>
+      <NoteList />
+    </div>
+  );
+}
+
+export default App;
